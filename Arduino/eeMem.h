71dd579fea0e67b16816dc6f9d3f8dab2d9da3d0
Greg Cunningham <greg@curioustech.net>
2023-01-26 06:46:50 -0500

    Merged structs

--- Arduino/eeMem.h
+++ Arduino/eeMem.h
@@ -9,7 +9,6 @@ struct Sched
   uint16_t timeSch;
   uint8_t thresh;
   uint8_t wday;  // Todo: weekday 0=any, 1-7 = day of week
-  char    name[16]; // names for small display
 };
 
 struct Alarm
@@ -22,38 +21,7 @@ struct Alarm
 
 #define MAX_SCHED 8
 
-struct eeSet // EEPROM backed data
-{
-  uint16_t size;          // if size changes, use defauls
-  uint16_t sum;           // if sum is diiferent from memory struct, write
-  char     szSSID[32];
-  char     szSSIDPassword[64];
-  uint16_t vacaTemp;     // vacation temp
-  int8_t  tz;            // Timezone offset from your global server
-  uint8_t schedCnt;      // number of active scedules
-  bool    bVaca;         // vacation enabled
-  bool    bAvg;          // average target between schedules
-  bool    bEco;          // eco mode
-  Sched   schedule[MAX_SCHED];  // 22x8 bytes
-  uint16_t ppkwh;
-  uint16_t rate;
-  uint16_t watts;
-  uint16_t ppkwm[12];
-  uint32_t tSecsMon[12];    // total secwatt hours per month
-  int16_t tAdj[2];
-  int16_t pids[3]; // Todo: real PID
-  uint8_t  hostIP[4];
-  uint16_t hostPort;
-  uint8_t  statIP[4];
-  uint16_t statPort;
-  uint8_t  lightIP[2][4];
-  uint8_t  resIP[4];
-  uint16_t resPort;
-  uint32_t nOvershootTime;
-  int16_t nOvershootTempDiff;
-  int8_t res[26];
-  Alarm   alarm[MAX_SCHED];
-}; // 620
+#define EESIZE (offsetof(eeMem, end) - offsetof(eeMem, size) )
 
 class eeMem
 {
@@ -63,8 +31,63 @@ public:
   bool verify(bool bComp);
 private:
   uint16_t Fletcher16( uint8_t* data, int count);
-};
+public:
+  uint16_t size = EESIZE;          // if size changes, use defauls
+  uint16_t sum = 0xAAAA;           // if sum is diiferent from memory struct, write
+  char     szSSID[32] = "";
+  char     szSSIDPassword[64] = "";
+  uint16_t vacaTemp = 700;     // vacation temp
+  int8_t  tz = -5;            // Timezone offset from your global server
+  uint8_t schedCnt[2] = {5,5};   // number of active scedules
+  bool    bVaca = false;         // vacation enabled
+  bool    bAvg = true;          // average target between schedules
+  bool    bEco = false;          // eco mode
+  Sched   schedule[2][MAX_SCHED] =  // 2x22x8 bytes
+  {
+    {
+      {831,  3*60, 3, 0},
+      {824,  6*60, 2, 0},  // temp, time, thresh, wday
+      {819,  8*60, 3, 0},
+      {819, 16*60, 3, 0},
+      {834, 21*60, 3, 0},
+      {830,  0*60, 3, 0},
+      {830,  0*60, 3, 0},
+      {830,  0*60, 3, 0}
+    },
+    {
+      {831,  3*60, 3, 0},
+      {824,  6*60, 2, 0},  // temp, time, thresh, wday
+      {819,  8*60, 3, 0},
+      {819, 16*60, 3, 0},
+      {834, 21*60, 3, 0},
+      {830,  0*60, 3, 0},
+      {830,  0*60, 3, 0},
+      {830,  0*60, 3, 0}
+    },
+  };
+  uint16_t ppkwh = 164; // $0.164 / KWH
+  uint16_t rate = 50; // seconds
+  uint16_t watts = 290; // Heating pad
+  uint16_t ppkwm[12] = {153,153,153,125,125,125,125,146,146,153,154,154}; // ppkwh per month
+  uint32_t tSecsMon[12] = {1254411,1067144,916519,850686,122453,268488,302535,396531,501161,552347,427980,883172}; // total secwatt hours per month (copied from page)
+  int16_t  tAdj[2] = {0,0};
+  int16_t  pids[3] = {60*3,60*1, 5}; // Todo: real PID
+  uint8_t  hostIP[4] = {192,168,31,100};
+  uint16_t hostPort = 80;
+  uint8_t  hvacIP[4] = {192,168,31,46};
+  uint16_t hvacPort = 80;
+  uint8_t  lightIP[2][4] = {{192,168,31,8}, {0,0,0,0}};
+  uint8_t  resIP[4]; // reserved for later
+  uint16_t resPort = 80;
+  uint32_t nOvershootTime;
+  int16_t  nOvershootTempDiff;
+  Alarm   alarm[MAX_SCHED] = 
+  { // alarms
+    {0, 1000, 8*60, 0x3E},
+    {0},
+  };
+  uint8_t end;
+}; // 392
 
-extern eeMem eemem;
-extern eeSet ee;
+extern eeMem ee;
 #endif // EEMEM_H
